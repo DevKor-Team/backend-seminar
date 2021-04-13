d5773afde5bbecd35950a410ba61e779d08b3540
hanchchch <cndghks15@gmail.com>
2021-04-13 22:14:07 +0900

    feat: CBV for list view

--- posts/urls.py
+++ posts/urls.py
@@ -3,7 +3,5 @@ from django.urls import path
 from posts import views
 
 urlpatterns = [
-    path('', views.index),
-    path('create/', views.create),
-    path('<int:pk>/', views.post),
+    path('', views.PostListCreateView.as_view()),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,52 +1,15 @@
-from django.shortcuts import render
-from django.http.response import JsonResponse
-from django.http import QueryDict
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
+from posts.serializer import PostSerializer
 from posts.services import all_posts, get_post, create_post, update_post, delete_post
 
-def index(request):
-  posts = all_posts()
-  data = [{
-    "id": post.id,
-    "title": post.title,
-    "contents": post.contents
-  } for post in posts]
+class PostListCreateView(APIView):
+  authentication_classes = []
+  permission_classes = []
 
-  return JsonResponse(data, safe=False)
+  queryset = all_posts()
 
-def detail(request, pk=None):
-  post = get_post(id=pk)
-  if post is None: return JsonResponse({}, status=404)
-  return JsonResponse({
-    "title": post.title,
-    "contents": post.contents,
-  })
-
-def create(request):
-  if request.method == "POST":
-    title = request.POST.get("title")
-    contents = request.POST.get("contents")
-    if not title: return JsonResponse({}, status=400)
-    create_post(title, contents)
-    return JsonResponse({"success": True})
-
-def update(request, pk=None):
-    post = update_post(
-      id=pk,
-      title=QueryDict(request.body).get("title"),
-      contents=QueryDict(request.body).get("contents")
-    )
-    if post is None: return JsonResponse({}, status=404)
-    return JsonResponse({"success": True})
-
-def delete(request, pk=None):
-    success = delete_post(pk)
-    return JsonResponse({"success": success})
-
-def post(request, pk=None):
-  if request.method == "GET":
-    return detail(request, pk)
-  if request.method == "PATCH":
-    return update(request, pk)
-  if request.method == "DELETE":
-    return delete(request, pk)
+  def get(self, request):
+    serializer = PostSerializer(self.queryset.all(), many=True)
+    return Response(serializer.data)
