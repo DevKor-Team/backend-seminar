ef2d20e0c8ae4d9600c7c8adf3496e0312a54362
hanchchch <cndghks15@gmail.com>
2021-05-03 00:46:30 +0900

    feat: user SignUpView

--- user/views.py
+++ user/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from django.http import Http404
 
-# Create your views here.
+from user.services import authenticate, login, create_user
+from user.decorators import require_username_password
+
+class SignUpView(APIView):
+  authentication_classes = []
+  permission_classes = []
+
+  @require_username_password
+  def post(self, request, username, password):
+    user = create_user(username, password)
+
+    if user is None:
+      return Response({"success": False}, status=status.HTTP_400_BAD_REQUEST)
+
+    login(request, user)
+
+    return Response({"success": True})
+
