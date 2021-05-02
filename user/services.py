9363cc2bcfb9a7ab1b68565b948356b37c23659c
hanchchch <cndghks15@gmail.com>
2021-05-03 00:46:05 +0900

    feat: user services

---
+++ user/services.py
@@ -0,0 +1,8 @@
+from django.contrib.auth import authenticate, login
+from user.models import User
+
+def create_user(username, password):
+  try:
+    return User.objects.create_user(username=username, password=password)
+  except:
+    return None
