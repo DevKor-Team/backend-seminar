88afee58ff4fa7cf8d6762b9e992831fc36cb4aa
hanchchch <cndghks15@gmail.com>
2021-05-03 00:45:50 +0900

    feat: require_uesrname_password decorator

---
+++ user/decorators.py
@@ -0,0 +1,12 @@
+from rest_framework.response import Response
+from rest_framework import status
+
+def require_username_password(func):
+  # for CBV methods
+  def wrapper(self, request, *args, **kwargs):
+    username = request.data.get("username")
+    password = request.data.get("password")
+    if (username is None) or (password is None):
+      return Response({"success": False, "error": "key error."}, status=status.HTTP_400_BAD_REQUEST)
+    return func(self, request, username, password, *args, **kwargs)
+  return wrapper
