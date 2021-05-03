bacc0d19212d2e518bf6b8123691524e71036314
hanchchch <cndghks15@gmail.com>
2021-05-03 09:21:10 +0900

    misc: indent

--- posts/views.py
+++ posts/views.py
@@ -34,19 +34,19 @@ class PostDetailView(APIView):
     return post
 
   def get(self, request, pk, format=None):
-      post = self.get_object(pk)
-      serializer = PostSerializer(post)
-      return Response(serializer.data)
+    post = self.get_object(pk)
+    serializer = PostSerializer(post)
+    return Response(serializer.data)
 
   def put(self, request, pk, format=None):
-      post = self.get_object(pk)
-      serializer = PostSerializer(post, data=request.data)
-      if serializer.is_valid():
-          serializer.save()
-          return Response(serializer.data)
-      return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    post = self.get_object(pk)
+    serializer = PostSerializer(post, data=request.data)
+    if serializer.is_valid():
+      serializer.save()
+      return Response(serializer.data)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
   def delete(self, request, pk, format=None):
-      post = self.get_object(pk)
-      post.delete()
-      return Response(status=status.HTTP_204_NO_CONTENT)
+    post = self.get_object(pk)
+    post.delete()
+    return Response(status=status.HTTP_204_NO_CONTENT)
