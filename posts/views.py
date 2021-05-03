3fb9d345039d088e9cb95242f2a3e48b28e96325
hanchchch <cndghks15@gmail.com>
2021-05-03 10:46:06 +0900

    fix: use GenericViewSet

--- posts/views.py
+++ posts/views.py
@@ -1,26 +1,21 @@
-from rest_framework.viewsets import ViewSet
-from rest_framework import decorators
+from rest_framework import decorators, status, mixins
+from rest_framework.viewsets import GenericViewSet
 from rest_framework.response import Response
-from rest_framework import status
 from django.http import Http404
 
 from posts.serializer import PostSerializer
 from posts.services import all_posts, get_post, create_post, update_post, delete_post
 
-class PostViewSet(ViewSet):
+class PostViewSet(GenericViewSet):
     authentication_classes = []
     permission_classes = []
 
+    serializer_class = PostSerializer
+
     queryset = all_posts()
 
-    def get_object(self, pk):
-        post = get_post(pk)
-        if post is None:
-            raise Http404
-        return post
-
     def list(self, request, *args, **kwargs):
-        serializer = PostSerializer(self.queryset.all(), many=True)
+        serializer = self.get_serializer(self.get_queryset(), many=True)
         return Response(serializer.data)
 
     def create(self, request, *args, **kwargs):
@@ -32,26 +27,26 @@ class PostViewSet(ViewSet):
         return Response({"success": True})
 
     def retrieve(self, request, pk=None, *args, **kwargs):
-        post = self.get_object(pk)
-        serializer = PostSerializer(post)
+        post = self.get_object()
+        serializer = self.get_serializer(post)
         return Response(serializer.data)
 
     def update(self, request, pk=None, *args, **kwargs):
-        post = self.get_object(pk)
-        serializer = PostSerializer(post, data=request.data, partial=True)
+        post = self.get_object()
+        serializer = self.get_serializer(post, data=request.data, partial=True)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def destroy(self, request, pk=None, *args, **kwargs):
-        post = self.get_object(pk)
+        post = self.get_object()
         post.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
     @decorators.action(methods=["GET", "POST"], detail=True)
     def like(self, request, pk=None, *args, **kwargs):
-        post = self.get_object(pk)
+        post = self.get_object()
         if request.method == "GET":
             return Response({"like": post.likes})
         elif request.method == "POST":
